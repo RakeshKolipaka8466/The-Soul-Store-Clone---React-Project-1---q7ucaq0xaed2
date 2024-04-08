56ec26a7446b94ce38fe48aac698fa3b35e9e088
Rakesh <rakeshkolipka8466@gmail.com>
2024-04-08 11:21:35 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    // screens: {
+    //   'mobile': {'min': '425px'},
+    //   'tablet': {'min': '768px'},
+    //   'desktop': {'min': '1024px'},
+    //   'large': {'min': '1440x'},
+    // }
   },
   plugins: [],
-};
+}
+
